468fdc9fa4a7bc000eab6ab82a17b7857f1a9772
Bogdan Dogaru <bogdan@vevolmedia.com>
2025-07-24 15:57:10 +0300

    vercel stuff

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+	// Handle preflight requests
+	if (req.method === 'OPTIONS') {
+		res.sendStatus(200);
+		return;
+	}
+
 	next();
 });
 
